0f3ca091b856afe5ef487284bf2837c755e795ab
nksb92 <kassubek41@gmail.com>
2025-12-21 21:30:43 +0100

    Add software timer config

---
+++ RLC_V3/soft_timer_cfg.cpp
@@ -0,0 +1,13 @@
+#include "soft_timer_cfg.h"
+
+// Timer Configuration Table
+const soft_timer_cfg_t soft_timer_config[] = {
+    // ID                      Name               DefPeriod                 Mode                  Event                     AutoStart
+    { TIMER_DISPLAY_STANDBY,  "DisplayStandby",  STD_STANDBY_TIME * 100,    TIMER_MODE_ONE_SHOT,  EVT_DISPLAY_STANDBY,      true  },
+    { TIMER_FAN_COOLDOWN,     "FanCooldown",     STD_FAN_RUN_ON_TIME * 100, TIMER_MODE_ONE_SHOT,  EVT_FAN_COOLDOWN,         false },
+    { TIMER_SCROLL_TEXT,      "ScrollText",      110,                       TIMER_MODE_PERIODIC,  EVT_SCROLL_TICK,          false },
+    { TIMER_CONNECTING_DOTS,  "ConnectingDots",  500,                       TIMER_MODE_PERIODIC,  EVT_CONNECTING_DOTS_TICK, false },
+    { TIMER_RAINBOW_ANIM,     "RainbowAnim",     100,                       TIMER_MODE_PERIODIC,  EVT_RAINBOW_TICK,         false },
+};
+
+const uint8_t soft_timer_config_count = sizeof(soft_timer_config) / sizeof(soft_timer_config[0]);
